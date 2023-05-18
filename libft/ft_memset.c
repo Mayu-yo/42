d7d37e4fd1e23d6cef5759ad75ee146accfc2607
mayyamad <mayyamad@student.42tokyo.jp>
2023-05-18 09:52:47 +0900

    2023/05/18

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,10 +1,24 @@
 #include "libft.h"
+#include <unistd.h>
+
+void *ft_memset(void *buf, int ch, size_t n);
 
 int main(){
-	ft_memset();
+	void *ret;
+	char ch[] = "123456789";
+	ret = ft_memset(ch, 3, 3);
+	write(1,ret,sizeof(ch)-1);
 	return 0;
 }
 
 void *ft_memset(void *buf, int ch, size_t n){
-	
+	unsigned char	*p = buf;
+	unsigned char uc = (unsigned char)ch;
+
+    while(n != 0){
+        *p = uc;
+		p++;
+		n--;
+    }
+    return (buf);
 }
