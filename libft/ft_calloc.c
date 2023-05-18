f6d1357f6abe90dd285414303b2c3f3e4f639b8c
mayyamad <mayyamad@student.42tokyo.jp>
2023-05-18 17:59:57 +0900

    2023/05/18

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -1,7 +1,30 @@
 #include "libft.h"
 
-int main(){
-	return 0;
+void *ft_calloc(size_t count, size_t size);
+#include <stdio.h>
+int main() {
+    // テスト用の配列の要素数と要素のサイズ
+    size_t numElements = 5;
+    size_t elementSize = sizeof(int);
+
+    // ft_calloc を使用してメモリを割り当て
+    int *arr = (int *)ft_calloc(numElements, elementSize);
+    if (arr == NULL) {
+        printf("メモリの割り当てに失敗しました。\n");
+        return 1;
+    }
+
+    // 配列の要素を表示
+    printf("配列の要素：");
+    for (size_t i = 0; i < numElements; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // メモリを解放
+    free(arr);
+
+    return 0;
 }
 
 void *ft_calloc(size_t count, size_t size){
--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,12 +1,12 @@
 #include "libft.h"
 
-int main(){
-	void *ret;
-	char ch[] = "123456789";
-	ret = ft_memset(ch, 100, 3);
-	write(1,ret,sizeof(ch)-1);
-	return 0;
-}
+// int main(){
+// 	void *ret;
+// 	char ch[] = "123456789";
+// 	ret = ft_memset(ch, 100, 3);
+// 	write(1,ret,sizeof(ch)-1);
+// 	return 0;
+// }
 
 void *ft_memset(void *buf, int ch, size_t n){
 	unsigned char	*p = buf;
