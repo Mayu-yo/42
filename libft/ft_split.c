a07e95477f8da76f5ef5877796d70ab62ec67a3c
Mayu Yamada <mr.obake0208@yahoo.com>
2023-06-07 12:44:12 +0900

    2023/06/07

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mayu <mayu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 14:14:39 by mayyamad          #+#    #+#             */
-/*   Updated: 2023/06/03 17:25:14 by mayu             ###   ########.fr       */
+/*   Updated: 2023/06/07 12:42:40 by mayu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,95 +23,61 @@ static char	**free_all(char **ret, int count)
 	return (NULL);
 }
 
-static char	**null_terminate_array(char **ret, int i, int j, int flag)
+static size_t	count_words(const char *str, char c)
 {
-	if (flag == 0)
-		ret[i++][j] = '\0';
-	ret[i] = NULL;
-	return (ret);
-}
+	size_t	len;
+	size_t	i;
 
-static int	count_word_len(const char *s, char c, size_t i, int which_len)
-{
-	int	count;
-	int	flag;
-
-	count = 0;
-	flag = 0;
-	while (s[i] != '\0')
+	len = 0;
+	i = 0;
+	while (str[i])
 	{
-		if (which_len == 0)
-		{
-			while (s[i++] != c && i < ft_strlen(s) && which_len == 0)
-				count++;
-			break ;
-		}
-		if (which_len == 1 && s[i] != c && flag == 0)
-		{
-			count++;
-			flag = 1;
-		}
-		if (which_len == 1 && s[i] == c)
-			flag = 0;
+		if ((str[i + 1] == c || str[i + 1] == '\0') && str[i] != c)
+			len++;
 		i++;
 	}
-	return (count);
+	return (len);
 }
 
-static char	**ft_split2(char const *s, char c, size_t k, char **ret)
+static size_t	ft_count_len(const char *str, char c)
 {
-	size_t		i;
-	size_t		j;
+	size_t	len;
 
-	i = 0;
-	j = 0;
-	while (s[k] != '\0')
+	len = 0;
+	while (*str != c && *str != '\0')
 	{
-		if (s[k] == c)
-		{
-			while (s[k] == c)
-				k++;
-			ret[i++][j] = '\0';
-			j = 0;
-			if (k == ft_strlen(s))
-				return (null_terminate_array(ret, i, j, 1));
-			else
-				ret[i] = malloc(count_word_len(s, c, k, 0) + 2);
-			if (ret[i] == NULL)
-				return (free_all(ret, i - 1));
-		}
-		if (s[k] != c && k < ft_strlen(s))
-			ret[i][j++] = s[k];
-		k++;
+		len++;
+		str++;
 	}
-	return (null_terminate_array(ret, i, j, 0));
+	return (len);
 }
 
-char	**ft_split(char const *s, char c)
+char	**ft_split(const char *s, char c)
 {
-	size_t	i;
+	size_t	k;
+	size_t	word_len;
+	size_t	words;
 	char	**ret;
 
-	i = 0;
+	k = 0;
 	if (!s)
 		return (NULL);
-	ret = malloc((count_word_len(s, c, i, 1) + 1) * sizeof(char *));
-	if (ret == NULL)
+	words = count_words(s, c);
+	ret = malloc((words + 1) * sizeof(char *));
+	if (!ret)
 		return (NULL);
-	if (s[0] == '\0')
-		return (null_terminate_array(ret, 0, 0, 1));
-	while (s[i] == c)
-		i++;
-	if (i == ft_strlen(s))
-		return (null_terminate_array(ret, 0, 0, 1));
-	ret[0] = malloc((count_word_len(s, c, i, 0) + 2) * sizeof(char));
-	if (ret[0] == NULL)
-		return (free_all(ret, 0));
-	ret = ft_split2(s, c, i, ret);
+	while (k < words)
+	{
+		while (*s == c && *s)
+			s++;
+		word_len = ft_count_len(s, c);
+		ret[k] = malloc((word_len + 1) * sizeof(char));
+		if (!ret[k])
+			return (free_all(ret, k));
+		ft_strlcpy(ret[k], s, word_len + 1);
+		s += word_len;
+		k++;
+	}
+	ret[k] = NULL;
 	return (ret);
 }
-
-// int main (){
-// 	ft_split("lorem i.", 'z');
-// 	return 0;
-// }
