eb7755313a99bf1e2e1851fdbdaa75a33259cabc
Mayu Yamada <mr.obake0208@yahoo.com>
2023-08-04 16:43:55 +0900

    2023/08/04

--- fractol/ft_atof.c
+++ fractol/ft_atof.c
@@ -8,32 +8,37 @@ double ft_atof(char *num){
 
 	minus_flag = 1;
 	i = 0;
-	j = 0;
+	j = 1;
 	ret = 0;
 	if (num[i] == '-'){
 		minus_flag = -1;
 		i++;
 	}
-	while ('0' <= num[i] || num[i] <= '9')
+	while ('0' <= num[i] && num[i] <= '9')
 	{
 		ret = ret * 10 + num[i] - '0';
 		i++;
 	}
 	if (num[i] == '.'){
 		i++;
-		while ('0' <= num[i] || num[i] <= '9')
+		while ('0' <= num[i] && num[i] <= '9')
 		{
-			ret = ret + (num[i] - '0') * pow((1/ 10), j);
+			ret = ret + ((num[i] - '0') * pow((0.1), j));
 			i++;
 			j++;
 		}
 	}
-	return (ret);
+	return (ret * minus_flag);
 }
 
-#include <stdio.h>
-int main (){
-	double num;
-	num = ft_atof("0.5345");
-	printf("%f",num);
-}
+// #include <stdio.h>
+// int main (){
+// 	double num;
+// 	num = ft_atof("-0.5345");
+// 	printf("%f\n",num);
+// 	// for (int i = 0; i < 10; i++){
+// 	// 	num = pow((0.1), i);
+// 	// 	printf("%f\n",num);
+// 	// }
+// 	return 0;
+// }
