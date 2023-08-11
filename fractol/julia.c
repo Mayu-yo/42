34e933c3522729ecc8cc85aac4d8369185904c7f
Mayu Yamada <mr.obake0208@yahoo.com>
2023-08-12 03:13:24 +0900

    2023/08/12

--- fractol/julia.c
+++ fractol/julia.c
@@ -1,19 +1,19 @@
 
 #include "fractal.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 #define WIDTH 800
 #define HEIGHT 600
+#define MAX_ITER 130
 
-int julia_set(int x, int y, int max_iter, t_complex c, double zoom) {
+int julia_set(int x, int y, t_complex c, double zoom) {
 	double temp;
     int iter = 0;
     t_complex z;
 
-    z.real = (x - WIDTH / 2.0) * 4.0 / WIDTH;
-    z.imag = (y - HEIGHT / 2.0) * 4.0 / HEIGHT;
-    while (iter < max_iter && (z.real * z.real + z.imag * z.imag) < 4.0) {
+    z.real = (x - WIDTH / 2.0) * 4.0 / WIDTH * zoom;
+    z.imag = (y - HEIGHT / 2.0) * 4.0 / HEIGHT * zoom;
+    while (iter < MAX_ITER && (z.real * z.real + z.imag * z.imag) < 4.0) {
         temp = z.real * z.real - z.imag * z.imag + c.real;
         z.imag = 2.0 * z.real * z.imag + c.imag;
         z.real = temp;
@@ -28,19 +28,13 @@ int julia_set_color(int iter)
     int color;
     int pixel_color;
 
-	color = iter * 255 / 100;
+	color = iter * 255 / MAX_ITER;
 	pixel_color = (color << 16) | (color << 8) | color;
 	return (pixel_color);
 }
 
-void draw_julia(t_data *img, t_complex c, double zoom){
-	// for (int y = 0; y < HEIGHT; y++) {
-    //     for (int x = 0; x < WIDTH; x++) {
-    //         int iter = julia_set(x, y, 100, c, zoom);
-    //         mlx_pixel_put(img->mlx_ptr, img->win_ptr, x, y, julia_set_color(iter));
-	// 		// my_mlx_pixel_put(&img, x, y, julia_set_color(iter));
-    //     }
-    // }
+void draw_julia(t_data *img, t_complex c, double zoom)
+{
 	int x = 0;
 	int y = 0;
 	int iter = 0;
@@ -50,7 +44,7 @@ void draw_julia(t_data *img, t_complex c, double zoom){
 		x = 0;
 		while (x < WIDTH)
 		{
-			iter = julia_set(x, y, 100, c, zoom);
+			iter = julia_set(x, y, c, zoom);
 			mlx_pixel_put(img->mlx_ptr, img->win_ptr, x, y, julia_set_color(iter));
 			x++;
 		}
@@ -58,7 +52,8 @@ void draw_julia(t_data *img, t_complex c, double zoom){
 	}
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     if (argc != 3) {
         printf("Usage: %s <real_part_of_c> <imaginary_part_of_c>\n", argv[0]);
         return 1;
@@ -67,11 +62,8 @@ int main(int argc, char *argv[]) {
     t_complex c;
     c.real = ft_atof(argv[1]);
     c.imag = ft_atof(argv[2]);
-
-    // mlx_init();
-    // void *mlx = mlx_init();
-    // void *win = mlx_new_window(mlx, WIDTH, HEIGHT, "Julia Set");
 	t_data img;
+
 	img.fractal = 2;
 	img.c = c;
 	img.mlx_ptr = mlx_init();
