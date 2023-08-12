93f9e8a02de6c41a595b0149db63b5f6a05df0d1
Mayu Yamada <mr.obake0208@yahoo.com>
2023-08-12 11:25:41 +0900

    2023/08/12

--- fractol/julia.c
+++ fractol/julia.c
@@ -11,8 +11,8 @@ int julia_set(int x, int y, t_complex c, double zoom) {
     int iter = 0;
     t_complex z;
 
-    z.real = (x - WIDTH / 2.0) * 4.0 / WIDTH * zoom;
-    z.imag = (y - HEIGHT / 2.0) * 4.0 / HEIGHT * zoom;
+    z.real = (x - WIDTH / 2.0) * 4.0 / WIDTH / zoom;
+    z.imag = (y - HEIGHT / 2.0) * 4.0 / HEIGHT / zoom;
     while (iter < MAX_ITER && (z.real * z.real + z.imag * z.imag) < 4.0) {
         temp = z.real * z.real - z.imag * z.imag + c.real;
         z.imag = 2.0 * z.real * z.imag + c.imag;
@@ -23,14 +23,26 @@ int julia_set(int x, int y, t_complex c, double zoom) {
     return iter;
 }
 
+// int julia_set_color(int iter)
+// {
+//     int color;
+//     int pixel_color;
+
+// 	color = iter * 255 / MAX_ITER;
+// 	pixel_color = (color << 16) | (color << 8) | color;
+// 	return (pixel_color);
+// }
 int julia_set_color(int iter)
 {
-    int color;
-    int pixel_color;
+    int red, green, blue;
 
-	color = iter * 255 / MAX_ITER;
-	pixel_color = (color << 16) | (color << 8) | color;
-	return (pixel_color);
+    // Calculate color components based on iteration and main color
+    red = (int)(64 + 0.3 * iter * (255 - 64) / MAX_ITER);
+    green = (int)(64 + 0.5 * iter * (255 - 64) / MAX_ITER);
+    blue = (int)(225 - 0.3 * iter * (225 - 64) / MAX_ITER);
+
+    int pixel_color = (red << 16) | (green << 8) | blue;
+    return pixel_color;
 }
 
 void draw_julia(t_data *img, t_complex c, double zoom)
@@ -51,6 +63,8 @@ void draw_julia(t_data *img, t_complex c, double zoom)
 		y++;
 	}
 }
+
+
 
 int main(int argc, char *argv[])
 {
