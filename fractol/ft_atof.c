7116c2d9aec619a491792fc81b79348fdcf259c9
Mayu Yamada <mr.obake0208@yahoo.com>
2023-08-15 11:27:22 +0900

    2023/08/15

--- fractol/ft_atof.c
+++ fractol/ft_atof.c
@@ -6,7 +6,7 @@
 /*   By: mayu <mayu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 22:11:02 by mayu              #+#    #+#             */
-/*   Updated: 2023/08/15 02:52:14 by mayu             ###   ########.fr       */
+/*   Updated: 2023/08/15 11:27:02 by mayu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static void	error_message(void)
 	exit (0);
 }
 
-double	calc_decimal_point(char *num, int integer, int minus_flag)
+double	calc_decimal_point(char *num, int integer, int negative)
 {
 	int		i;
 	double	digit;
@@ -36,57 +36,61 @@ double	calc_decimal_point(char *num, int integer, int minus_flag)
 		i++;
 	}
 	if (num[i] == '\0')
-		return (ret * minus_flag);
+		return (ret * negative);
 	else
 		error_message();
 	exit (0);
 }
 
-double	calc_int_part(char *num)
+double	calc_int_part(char *num, int negative)
 {
 	size_t	i;
 	double	ret;
-	int		minus_flag;
 
 	i = 0;
 	ret = 0;
-	minus_flag = 1;
-	if (num[i] == '-')
-	{
-		minus_flag = -1;
-		i++;
-	}
+	
 	while ('0' <= num[i] && num[i] <= '9')
 	{
 		ret = ret * 10 + num[i] - '0';
+		if ((ret > INT_MAX && negative == 1) || (ret - 1 > INT_MAX && negative == -1))
+			error_message();
 		i++;
 	}
 	if (num[i] == '.' && i > 0 && ('0' <= num[i - 1] && num[i - 1] <= '9'))
-		ret = calc_decimal_point(&num[i + 1], ret, minus_flag);
+		ret = calc_decimal_point(&num[i + 1], ret, negative);
 	else if (num[i] == '\0')
-		return (ret * minus_flag);
+		return (ret * negative);
 	else
 		error_message();
-	return (ret);
+	return (ret * negative);//なんで2回入るんだろ
 }
 
 double	ft_atof(char *num)
 {
+	int		negative;
 	double	ret;
 
 	ret = 0;
-	ret = calc_int_part(num);
+	negative = 1;
+	if (num[0] == '-')
+	{
+		negative = -1;
+		ret = calc_int_part(&num[1], negative);
+	}
+	else
+		ret = calc_int_part(num, negative);
 	return (ret);
 }
 
-// #include <stdio.h>
-// int main (){
-// 	double num;
-// 	num = ft_atof("0.125");
-// 	printf("%f\n",num);
-// 	// for (int i = 0; i < 10; i++){
-// 	// 	num = pow((0.1), i);
-// 	// 	printf("%f\n",num);
-// 	// }
-// 	return 0;
-// }
+#include <stdio.h>
+int main (){
+	double num;
+	num = ft_atof("2147483647.125");
+	printf("%f\n",num);
+	// for (int i = 0; i < 10; i++){
+	// 	num = pow((0.1), i);
+	// 	printf("%f\n",num);
+	// }
+	return 0;
+}
