633eb6f3e7b3654d8df8ce12fdd5e84b0b6742f8
myama <m.yamada@exafactory.co.jp>
2024-08-27 10:00:25 +0900

    philo main

--- philosophers/main.c
+++ philosophers/main.c
@@ -6,17 +6,18 @@ int main (int argc, char **argv)
 	t_philo			philos;
 
 	/* ----------------- vscode debug ------------------ */																	
-	argc = 6;
-	argv[1] = "5";
-	argv[2] = "800";
-	argv[3] = "200";
-	argv[4] = "200";
-	argv[5] = "7";
+	// argc = 6;
+	// argv[1] = "5";
+	// argv[2] = "800";
+	// argv[3] = "200";
+	// argv[4] = "200";
+	// argv[5] = "7";
 	/* ------------------------------------------------- */
 
 	input_check(argc, argv);
 	init(argc, argv, &settings, &philos);
 	printf("init success\n");
+	printf("%d" settings->philos[i].id);
 	thread_init(&philos, &settings);
 	thread_destroy(&philos, &settings);
 	
