30233658e0e2f3a3e41109d1c8f6f464deb9c659
myama <m.yamada@exafactory.co.jp>
2024-12-04 12:45:29 +0900

    cpp01 start

---
+++ cpp/cpp01/ex00/Zombie.hpp
@@ -0,0 +1,20 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+
+class Zombie
+{
+	private:
+		std::string _name;
+
+	public:
+		Zombie();
+		~Zombie();
+		void set_name(std::string str);
+		void announce(void);
+        Zombie *newZombie(std::string name);
+        void    randomChump(std::string name);
+};
+
+#endif
---
+++ cpp/cpp01/ex00/main.cpp
@@ -0,0 +1,5 @@
+#include "Zombie.hpp"
+
+int main(void){
+    return (0);
+}
